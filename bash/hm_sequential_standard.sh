8da0d8211238973f99979800d2fabf2a5f1a3e13
kfang <kfang11@asu.edu>
2024-05-17 15:14:35 -0700

    update3pm

--- bash/hm_sequential_standard.sh
+++ bash/hm_sequential_standard.sh
@@ -11,7 +11,7 @@ for j in 1 2 3 5 7 10
                     do
                         for SplitBendersStrategy in "NO_SPLIT_BENDERS_STRATEGY" "ALL_SPLIT_BENDERS_STRATEGY" "TIGHT_SPLIT_BENDERS_STRATEGY" 
                         do
-                            bash bash/cfl_sequential.sh "f$(j)00-c(j)00-r5.0-p$i" $cut_strategy $SplitCGLPNormType $SplitSetSelectionPolicy $StrengthenCutStrategy $SplitBendersStrategy
+                            bash bash/cfl_sequential.sh "f$(j)00-c$(j)00-r5.0-p$i" $cut_strategy $SplitCGLPNormType $SplitSetSelectionPolicy $StrengthenCutStrategy $SplitBendersStrategy
                         done
                     done
                 done
--- test/test_cfl_mip.jl
+++ test/test_cfl_mip.jl
@@ -5,7 +5,7 @@ using JuMP, CSV, CPLEX, Gurobi
 # instance = "f50-c50-r5.0-p1"
 # data = DisjunctiveBenders.read_random_data(instance)
 
-instance = "p30"
+instance = "p35"
 data = SplitBenders.read_data(instance)
 @info instance
 
