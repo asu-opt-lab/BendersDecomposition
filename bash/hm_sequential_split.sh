5df176ab235eb48d1960e508dc3b1ba9cc1dec78
ASUKaiwenFang <kfang11@asu.edu>
2024-05-20 09:51:40 -0700

    update10am

---
+++ bash/hm_sequential_split.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#SBATCH -t 0-01:00:00
+
+
+for ((i=1; i<=1; i++))
+do
+    for cut_strategy in "ORDINARY_CUTSTRATEGY" "SPLIT_CUTSTRATEGY"
+    do
+        for SplitCGLPNormType in "STANDARDNORM" "L1GAMMANORM" "LINFGAMMANORM"
+        do
+            for SplitSetSelectionPolicy in "MOST_FRAC_INDEX" "RANDOM_INDEX"
+            do
+                for StrengthenCutStrategy in "SPLIT_PURE_CUT_STRATEGY" "SPLIT_STRENGTHEN_CUT_STRATEGY" 
+                do
+                    for SplitBendersStrategy in "NO_SPLIT_BENDERS_STRATEGY" "ALL_SPLIT_BENDERS_STRATEGY" "TIGHT_SPLIT_BENDERS_STRATEGY" 
+                    do
+                        bash bash/cfl_sequential.sh "f100-c100-r5.0-p$i" $cut_strategy $SplitCGLPNormType $SplitSetSelectionPolicy $StrengthenCutStrategy $SplitBendersStrategy
+                    done
+                done
+            done
+        done
+    done
+done
