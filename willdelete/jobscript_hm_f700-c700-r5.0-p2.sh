146f610b89e31325f4530b384649e4ea7a955490
ASUKaiwenFang <kfang11@asu.edu>
2024-07-11 17:07:39 -0700

    update

---
+++ willdelete/jobscript_hm_f700-c700-r5.0-p2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 28
+#SBATCH -t 0-02:00:00
+#SBATCH -o willdelete/hm.shf700-c700-r5.0-p2.out%j
+#SBATCH -e willdelete/hm.shf700-c700-r5.0-p2.err%j
+module purge
+module load julia
+module load cplex
+module load gurobi
+julia --project=. bash/0_cfl_sequential_split.jl --instance f700-c700-r5.0-p2
