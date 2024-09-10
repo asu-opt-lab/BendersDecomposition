21da28be6e9c8e730794d1aece51e8f6e4df788a
kfang <kfang11@asu.edu>
2024-09-09 23:20:21 -0700

    update

--- src/Algorithms/Cut/Advanced.jl
+++ src/Algorithms/Cut/Advanced.jl
@@ -1,5 +1,50 @@
 
 
+# function generate_cut(
+#     master_env::AbstractMasterEnv, 
+#     sub_env::AbstractSubEnv, 
+#     ::AdvancedCutStrategy;
+#     time_limit::Float64 = 1000.00)
+
+#     cVal = master_env.value_x
+#     l = length(cVal)    
+#     for i in eachindex(cVal)
+#         set_normalized_rhs(sub_env.cconstr[i], cVal[i])
+#         set_normalized_rhs(sub_env.cconstr[l+i], -cVal[i])
+#     end
+#     set_normalized_rhs(sub_env.oconstr, -master_env.value_t)
+
+#     # set_time_limit_sec(sub_env.model, max(time_limit,100))
+#     start_time = time()
+#     JuMP.optimize!(sub_env.model)
+#     sub_time = time() - start_time
+
+#     status = dual_status(sub_env.model)
+#     # update value_η!!!!!!!!!!!!!!!!!!!
+#     if status == FEASIBLE_POINT
+#         subObjVal = JuMP.objective_value(sub_env.model) 
+#         ex = @expression(master_env.model,  
+#         subObjVal 
+#         - dual(sub_env.oconstr)*(master_env.var["t"] - master_env.value_t)
+#         - sum(dual(sub_env.cconstr[i]) * (master_env.var["cvar"][i] - cVal[i]) for i in eachindex(cVal)) 
+#         - sum(dual(sub_env.cconstr[l+i]) * (master_env.var["cvar"][i] - cVal[i]) for i in eachindex(cVal)))       
+#     else
+#         @error "dual of sub is neither feasible nor infeasible certificate: $status"
+#         throw(-1)
+#     end
+
+#     if abs(subObjVal) <= 1e-06
+#         sub_env.obj_value = value(sub_env.obj)#sum(data.costs[i,j] * data.demands[j] * JuMP.value(sub_env.model[:y][i,j]) for i in 1:data.n_facilities, j in 1:data.n_customers)  
+#     else 
+#         sub_env.obj_value = Inf
+#     end
+
+#     return sub_time,ex
+# end
+
+
+
+
 function generate_cut(
     master_env::AbstractMasterEnv, 
     sub_env::AbstractSubEnv, 
@@ -10,7 +55,6 @@ function generate_cut(
     l = length(cVal)    
     for i in eachindex(cVal)
         set_normalized_rhs(sub_env.cconstr[i], cVal[i])
-        set_normalized_rhs(sub_env.cconstr[l+i], -cVal[i])
     end
     set_normalized_rhs(sub_env.oconstr, -master_env.value_t)
 
@@ -21,27 +65,29 @@ function generate_cut(
 
     status = dual_status(sub_env.model)
     # update value_η!!!!!!!!!!!!!!!!!!!
-    if status == FEASIBLE_POINT
+    if status == FEASIBLE_POINT || status == INFEASIBILITY_CERTIFICATE
         subObjVal = JuMP.objective_value(sub_env.model) 
-        ex = @expression(master_env.model,  
-        subObjVal 
-        - dual(sub_env.oconstr)*(master_env.var["t"] - master_env.value_t)
-        - sum(dual(sub_env.cconstr[i]) * (master_env.var["cvar"][i] - cVal[i]) for i in eachindex(cVal)) 
-        - sum(dual(sub_env.cconstr[l+i]) * (master_env.var["cvar"][i] - cVal[i]) for i in eachindex(cVal)))       
+        @info subObjVal
+        # ex = @expression(master_env.model,  
+        # subObjVal 
+        # - dual(sub_env.oconstr)*(master_env.var["t"] - master_env.value_t)
+        # + sum(dual(sub_env.cconstr[i]) * (master_env.var["cvar"][i] - cVal[i]) for i in eachindex(cVal)))    
+        ex = @expression(master_env.model, -sum(dual.(sub_env.model[:cb])) + dual.(sub_env.cconstr)'master_env.var["cvar"] - dual(sub_env.oconstr)*master_env.var["t"])
+        # @info ex
+        # @info ex1
     else
         @error "dual of sub is neither feasible nor infeasible certificate: $status"
         throw(-1)
     end
 
-    if abs(subObjVal) <= 1e-06
-        sub_env.obj_value = value(sub_env.obj)#sum(data.costs[i,j] * data.demands[j] * JuMP.value(sub_env.model[:y][i,j]) for i in 1:data.n_facilities, j in 1:data.n_customers)  
-    else 
-        sub_env.obj_value = Inf
-    end
-
+    # if abs(subObjVal) <= 1e-06
+    #     sub_env.obj_value = value(sub_env.obj)#sum(data.costs[i,j] * data.demands[j] * JuMP.value(sub_env.model[:y][i,j]) for i in 1:data.n_facilities, j in 1:data.n_customers)  
+    # else 
+    #     sub_env.obj_value = Inf
+    # end
+    sub_env.obj_value = Inf
     return sub_time,ex
 end
-
 
 
 
--- src/Models/UFLP/SubProblem.jl
+++ src/Models/UFLP/SubProblem.jl
@@ -10,6 +10,18 @@ mutable struct UFLPStandardSubEnv <: AbstractSubEnv
     algo_params
 end
 
+mutable struct UFLPStandardADSubEnv <: AbstractSubEnv
+    model::Model
+    sub_constr::Array
+    sub_rhs::Array
+    cconstr::Array
+    obj_value::Float64
+    obj
+    oconstr
+
+    algo_params
+end
+
 mutable struct UFLPSplitSubEnv <: AbstractSubEnv
     model::Model
     sub_constr::Array
@@ -25,11 +37,19 @@ mutable struct UFLPSplitSubEnv <: AbstractSubEnv
 end
 
 
-function UFLPStandardSubEnv(data::UFLPData, algo_params; solver::Symbol=:Gurobi)
+function UFLPStandardSubEnv(data, algo_params; solver::Symbol=:Gurobi)
     
     model, constr, rhs, cconstr = generate_UFLP_subproblem(data, solver=solver) 
+    # model, constr, rhs, cconstr = generate_UFLP_subproblem_Advanced(data, solver=solver) 
 
     return UFLPStandardSubEnv(model, constr, rhs, cconstr, 0.0, algo_params)
+end
+
+function UFLPStandardADSubEnv(data, algo_params; solver::Symbol=:Gurobi)
+    
+    model, constr, rhs, cconstr, obj, oconstr = generate_UFLP_subproblem_Advanced(data, solver=solver) 
+
+    return UFLPStandardADSubEnv(model, constr, rhs, cconstr, 0.0, obj, oconstr, algo_params)
 end
 
 function UFLPSplitSubEnv(data::UFLPData, algo_params; solver::Symbol=:Gurobi)
@@ -100,7 +120,59 @@ function generate_UFLP_subproblem(data::Union{UFLPData,CFLPData}; solver::Symbol
     return model, constr, rhs, cconstr
 end
 
+# function generate_UFLP_subproblem_Advanced(data::UFLPData; solver::Symbol=:Gurobi)
+function generate_UFLP_subproblem_Advanced(data; solver::Symbol=:Gurobi)
+    if solver == :CPLEX
+        model = Model(CPLEX.Optimizer)
+        # set_optimizer_attribute(model, "CPX_PARAM_REDUCE", 0)
+    elseif solver == :Gurobi
+        model = Model(Gurobi.Optimizer)
+        # set_optimizer_attribute(model, "Method", 1)
+        set_optimizer_attribute(model, "InfUnbdInfo", 1)
+    end
+    set_optimizer_attribute(model, MOI.Silent(),true)
+    # set_time_limit_sec(model, 10)
+    # pre
+    N = data.n_facilities
+    M = data.n_customers
+    
+    @variable(model, y[1:data.n_facilities, 1:data.n_customers] >= 0)
 
+    cvar = Dict()
+    cvar["x"] = @variable(model, x[1:data.n_facilities])
+    @variable(model, σ)
+
+    @objective(model, Min, σ)
+
+    @constraint(model, cb[j in 1:M], -sum(y[i,j] for i in 1:N) + σ == -1)
+    # @constraint(model, c2[i in 1:N], sum(data.demands[j] * y[i,j] for j in 1:M) <= data.capacities[i] * cvar["x"][i])
+    @constraint(model, c3[i in 1:N, j in 1:M], -y[i,j] + σ >= -cvar["x"][i])
+
+    constr = []
+    rhs = []
+
+    all_affine_constraints = [all_constraints(model, GenericAffExpr{Float64,VariableRef}, MOI.GreaterThan{Float64});
+                              all_constraints(model, GenericAffExpr{Float64,VariableRef}, MOI.LessThan{Float64});
+                              all_constraints(model, GenericAffExpr{Float64,VariableRef}, MOI.EqualTo{Float64})]
+
+    for (i, c) in enumerate(all_affine_constraints)
+        push!(constr, c)
+        push!(rhs, normalized_rhs(c))
+    end
+
+    cconstr = []
+
+    for i in 1:data.n_facilities
+        push!(cconstr, @constraint(model, cvar["x"][i] == 0)) #x̂
+    end
+
+
+    obj = @expression(model, sum(data.costs[i,j] * data.demands[j] * y[i,j] for i in 1:data.n_facilities, j in 1:data.n_customers))
+    oconstr = @constraint(model, -obj + σ >= 0) #-η
+
+
+    return model, constr, rhs, cconstr, obj, oconstr
+end
 
 
 
