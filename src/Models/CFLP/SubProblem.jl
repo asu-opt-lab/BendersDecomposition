a3d633ccaca8bf7944b8176256dd92791db0f5a7
kfang <kfang11@asu.edu>
2024-09-18 12:20:03 -0700

    update knapsack

--- src/Models/CFLP/SubProblem.jl
+++ src/Models/CFLP/SubProblem.jl
@@ -69,6 +69,42 @@ end
 
 
 
+mutable struct CFLPStandardKNSubEnv <: AbstractSubEnv
+    model::Model
+    sub_constr::Array
+    sub_rhs::Array
+    cconstr::Array
+    obj_value::Float64
 
+    algo_params
+    data::CFLPData
+    knapsack_subproblems
+end
 
+function CFLPStandardKNSubEnv(data::CFLPData, algo_params; solver::Symbol=:Gurobi)
+    
+    model, constr, rhs, cconstr = generate_CFLP_subproblem(data, solver=solver) 
 
+    kanpsack_subproblems = generate_knapsack_subproblems(data)
+    return CFLPStandardKNSubEnv(model, constr, rhs, cconstr, 0.0, algo_params, data, kanpsack_subproblems)
+end
+
+#### Knapsack version
+function generate_knapsack_subproblems(data::CFLPData)
+    I = data.n_facilities
+    J = data.n_customers
+    models = Vector{Model}(undef, I)
+    u = zeros(J) 
+
+    for i in 1:I
+        model = Model(CPLEX.Optimizer)
+        set_optimizer_attribute(model, MOI.Silent(),true)
+        @variable(model, 0<=z[1:data.n_customers]<=1)
+        @objective(model, Min, sum((data.demands[j] * data.costs[i,j] - u[j]) * z[j] for j in 1:J)) 
+
+        @constraint(model, sum(data.demands[j] * z[j] for j in 1:J) <= data.capacities[i])
+
+        models[i] = model
+    end
+    return models
+end
