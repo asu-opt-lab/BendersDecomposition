3f2d3ca725a3c891fde734272e12e1ce8e98e75c
신인호 <sin-inho@macbookpro.dhcp.asu.edu>
2025-01-10 13:57:47 -0700

    FatKnapsackcut

--- src/models/UFLP/master_problem.jl
+++ src/models/UFLP/master_problem.jl
@@ -8,6 +8,7 @@ mutable struct UFLPMasterProblem <: AbstractUFLPMasterProblem
     var::Dict
     obj_value::Float64
     x_value::Vector{Float64}
+    t_value::Union{Vector{Float64}, Float64}
 end
 
 
@@ -17,6 +18,7 @@ function create_master_problem(data::UFLPData, cut_strategy::CutStrategy
     model = Model()
 
     N = data.n_facilities
+    M = data.n_customers
     @variable(model, x[1:N], Bin)
     
     t = create_t_variable(model, cut_strategy, data)
@@ -24,7 +26,11 @@ function create_master_problem(data::UFLPData, cut_strategy::CutStrategy
     @objective(model, Min, sum(data.fixed_costs .* x) + sum(t))
     @constraint(model, sum(x) >= 2)
     
-    return UFLPMasterProblem(model, Dict(:x => x, :t => t), 0.0, zeros(N))
+    if cut_strategy == FatKnapsackCut()
+        return UFLPMasterProblem(model, Dict(:x => x, :t => t), 0.0, zeros(N), zeros(M))
+    else
+        return UFLPMasterProblem(model, Dict(:x => x, :t => t), 0.0, zeros(N), 0.0)
+    end
 end
 
 
