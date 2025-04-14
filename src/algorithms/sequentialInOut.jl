531b81937aa39cd71ef2b065570dfdbc8ff980de
gbyeon <Geunyeong.Byeon@asu.edu>
2025-04-14 15:31:50 -0700

    done - typical oracle sequential

--- src/algorithms/sequentialInOut.jl
+++ src/algorithms/sequentialInOut.jl
@@ -1,106 +1,92 @@
 """
 Run BendersSeqInOut
 """
+# params should have stabilizing_x
 function solve!(env::BendersEnv, ::SeqInOut, params::BendersParams)
-log = BendersLog()
-try    
-    state = BendersState()
-    stabilizing_x = ones(env.data.dim_x)
-    α = 0.9
-    λ = 0.1
-    # relax_integrality(env.master.model)
-    prev_lb = -Inf
-    consecutive_no_improvement = 0
-    kelley_mode = false
-    
-    while true
-        state.iteration += 1
+    log = BendersSeqLog()
+    try    
+        state = BendersSeqState()
+        stabilizing_x = ones(env.data.dim_x)
+        α = 0.9
+        λ = 0.1
+        # relax_integrality(env.master.model)
+        kelley_mode = false
         
-        # Solve master problem
-        state.master_time = @elapsed begin
-            set_time_limit_sec(env.master.model, get_sec_remaining(log, params))
-            optimize!(env.master.model)
-            if is_solved_and_feasible(env.master.model; allow_local = false, dual = false)
-                env.master.obj_value = JuMP.objective_value(env.master.model)
-                env.master.x_value = value.(env.master.model[:x])
-                env.master.t_value = value.(env.master.model[:t])
-                state.LB = env.master.obj_value
-            elseif termination_status(env.master.model) == TIME_LIMIT
-                throw(TimeLimitException("Time limit reached during master solving"))
-            else 
-                throw(ErrorException("master termination status: $(termination_status(env.master.model))"))
-                # if infeasible, then the milp is infeasible
-            end
-        end
-        log.master_time += state.master_time
-        
-        # perturb point
-        stabilizing_x = α * stabilizing_x + (1 - α) * env.master.x_value
-        intermediate_x = λ * env.master.x_value + (1 - λ) * stabilizing_x
-
-        # Execute oracle
-        state.oracle_time = @elapsed begin
-            state.is_in_L, hyperplanes, sub_obj_val = generate_cuts(env.oracle, env.master.x_value, env.master.t_value; time_limit = get_sec_remaining(log, params))
-
-            cuts = !state.is_in_L ? hyperplanes_to_expression(env.master.model, hyperplanes, env.master.model[:x], env.master.model[:t]) : []
-
-            if kelley_mode && sub_obj_val != NaN
-                # Check termination criteria
-                update_upper_bound_and_gap!(state, env, sub_obj_val)
-            else
-                state.is_in_L = false
-            end
-        end
-        log.oracle_time += state.oracle_time
-
-        # Update state and record information
-        record_iteration!(log, state)
-
-        params.verbose && print_iteration_info(state, log)
-
-        is_terminated(state, params, log) && break
-        
-        if !kelley_mode && state.iteration > 1 && prev_lb != -Inf
-            lb_improvement = abs(prev_lb) < 1e-4 ? abs(state.LB - prev_lb) : abs((state.LB - prev_lb) / prev_lb) * 100
-            
-            # Check for improvement
-            if lb_improvement < 0.05
-                consecutive_no_improvement += 1
+        while true
+            state = BendersSeqState()
+            state.total_time = @elapsed begin
+                # Solve master problem
+                state.master_time = @elapsed begin
+                    set_time_limit_sec(env.master.model, get_sec_remaining(log, params))
+                    optimize!(env.master.model)
+                    if is_solved_and_feasible(env.master.model; allow_local = false, dual = false)
+                        state.LB = JuMP.objective_value(env.master.model)
+                        state.x_value = value.(env.master.model[:x])
+                        state.t_value = value.(env.master.model[:t])
+                    elseif termination_status(env.master.model) == TIME_LIMIT
+                        throw(TimeLimitException("Time limit reached during master solving"))
+                    else 
+                        throw(ErrorException("master termination status: $(termination_status(env.master.model))"))
+                        # if infeasible, then the milp is infeasible
+                    end
+                end
                 
-                # After 5 consecutive iterations without improvement
-                if consecutive_no_improvement == 5 && !kelley_mode
+                # perturb point
+                stabilizing_x = α * stabilizing_x + (1 - α) * state.x_value
+                intermediate_x = λ * state.x_value + (1 - λ) * stabilizing_x
+
+                # Execute oracle
+                state.oracle_time = @elapsed begin
+                    state.is_in_L, hyperplanes, state.f_x = generate_cuts(env.oracle, state.x_value, state.t_value; time_limit = get_sec_remaining(log, params))
+
+                    cuts = !state.is_in_L ? hyperplanes_to_expression(env.master.model, hyperplanes, env.master.model[:x], env.master.model[:t]) : []
+
+                    if kelley_mode 
+                        if state.f_x != NaN
+                            update_upper_bound_and_gap!(state, (f_x, x) -> env.data.c_t' * f_x + env.data.c_x' * x)
+                        end
+                    else
+                        state.is_in_L = false
+                    end
+                end
+                
+                # add generated cuts to master
+                @constraint(env.master.model, 0 .>= cuts)
+            
+                params.verbose && print_iteration_info(state, log)
+            end
+
+            # Update state and record information
+            record_iteration!(log, state)
+
+            # Check termination criteria
+            is_terminated(state, log, params) && break
+            
+            # whether to switch kelley mode
+            if !kelley_mode && log.n_iter != 0
+                check_lb_improvement!(state, log; zero_tol = 1e-8, tol_imprv = 0.05)
+
+                if log.consecutive_no_improvement >= 5
                     # Reset λ to 1 (switch to Kelley's cutting plane)
                     λ = 1.0
                     kelley_mode = true
-                    consecutive_no_improvement = 0
                     params.verbose && println("Switching to Kelley's cutting plane method (λ = 1.0)")
-                # After 5 more consecutive iterations without improvement in Kelley mode
-                # elseif consecutive_no_improvement == 5 && kelley_mode
-                #     params.verbose && println("Aborting after 5 consecutive iterations without improvement in Kelley mode")
-                #     break
                 end
-            else
-                # Reset counter if there's improvement
-                consecutive_no_improvement = 0
             end
         end
+        env.termination_status = Optimal()
+        env.obj_value = log.iterations[end].LB
         
-        prev_lb = state.LB
-
-        # Generate and add cuts
-        @constraint(env.master.model, 0 .>= cuts)
+        return to_dataframe(log)
+    catch e
+        if typeof(e) <: TimeLimitException
+            env.termination_status = TimeLimit()
+            env.obj_value = log.iterations[end].LB
+        elseif typeof(e) <: UnexpectedModelStatusException
+            env.termination_status = InfeasibleOrNumericalIssue()
+        else
+            rethrow()  
+        end
     end
-    env.log.termination_status = Optimal()
-    
-    return to_dataframe(log)
-catch e
-    if typeof(e) <: TimeLimitException
-        log.termination_status = TimeLimit()
-    elseif typeof(e) <: UnexpectedModelStatusException
-        log.termination_status = InfeasibleOrNumericalIssue()
-    else
-        rethrow()  
-    end
-    env.log = log
-end
+    # even if it terminates in the middle due to time limit, should be able to access the latest x_value via env.iterations[end].x_value
 end
---
+++ src/models/oracleTypical.jl
@@ -0,0 +1,61 @@
+export ClassicalOracle
+
+mutable struct ClassicalOracle <: AbstractTypicalOracle
+    model::Model
+    fixed_x_constraints::Vector{ConstraintRef}
+    other_constraints::Vector{ConstraintRef}
+
+    function ClassicalOracle(data::Data; scen_idx::Int=-1)
+        @debug "Building classical oracle"
+        model = Model()
+
+        # Define coupling variables and constraints
+        @variable(model, x[1:data.dim_x])
+        @constraint(model, fix_x, x .== 0)
+
+        other_constr = Vector{ConstraintRef}()
+
+        new(model, fix_x, other_constr)
+    end
+
+    ClassicalOracle() = new()
+end
+
+function generate_cuts(oracle::ClassicalOracle, x_value::Vector{Float64}, t_value::Vector{Float64}; tol = 1e-8, time_limit = 3600)
+    set_time_limit_sec(oracle.model, time_limit)
+    set_normalized_rhs.(oracle.fixed_x_constraints, x_value)
+    optimize!(oracle.model)
+    if termination_status(oracle.model) == TIME_LIMIT
+        throw(TimeLimitException("Time limit reached during cut generation"))
+    end
+    
+    status = dual_status(oracle.model)
+    if status == FEASIBLE_POINT
+        sub_obj_val = objective_value(oracle.model)
+
+        if sub_obj_val >= t_value[1] + tol
+            a_x = dual.(oracle.fixed_x_constraints) 
+            a_t = [-1.0] 
+            a_0 = sub_obj_val - a_x'*x_value 
+            return false, [Hyperplane(a_x, a_t, a_0)], [sub_obj_val]
+        end
+        
+        return true, [Hyperplane(length(x_value), length(t_value))], t_value
+
+    elseif status == INFEASIBILITY_CERTIFICATE
+        if has_duals(oracle.model)
+            a_x = dual.(oracle.fixed_x_constraints)
+            a_t = [0.0]
+            a_0 = dual.(oracle.other_constraints)' * normalized_rhs.(oracle.other_constraints)
+            return false, [Hyperplane(a_x, a_t, a_0)], [Inf]
+        end
+    else
+        throw(UnexpectedModelStatusException("ClassicalOracle: $(status)"))
+    end
+end
+
+
+
+
+
+
