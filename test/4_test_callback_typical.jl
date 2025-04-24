a5b54fb5126d9425cc7a0552bd3551ad30dd4f66
ASUKaiwenFang <kfang11@asu.edu>
2025-04-24 12:57:31 -0700

    update testing file

--- test/4_test_callback_typical.jl
+++ test/4_test_callback_typical.jl
@@ -236,7 +236,7 @@ include("$(dirname(@__DIR__))/example/uflp/model.jl")
 
 @testset verbose = true "UFLP Callback Benders Tests" begin
     # Specify instances to test
-    instances = 4:4  # For quick testing
+    instances = setdiff(1:71, [67])  # For quick testing
     
     for i in instances
         @testset "Instance: p$i" begin
@@ -297,11 +297,66 @@ include("$(dirname(@__DIR__))/example/cflp/model.jl")
 
 @testset verbose = true "CFLP Callback Benders Tests" begin
     # Specify instances to test
-    instances = 2:2  # For quick testing
+    instances = setdiff(1:71, [67])  # For quick testing
     
     for i in instances
         @testset "Instance: p$i" begin
-            @info "Testing CFLP instance $i"
+            @info "Testing CFLP easy instance $i"
+            
+            # Load problem data
+            problem = read_cflp_benchmark_data("p$i")
+            
+            # Get standard parameters
+            benders_param, dcglp_param, mip_solver_param, master_solver_param, 
+            typical_oracle_solver_param, dcglp_solver_param = get_standard_params()
+            
+            # Create data object
+            data = create_data(problem)
+            
+            # Solve MIP for reference
+            mip_opt_val = solve_reference_mip(data, mip_solver_param)
+            
+            # Standard test info
+            test_info = (mip_opt_val, master_solver_param, typical_oracle_solver_param)
+            
+            # Test classical oracle
+            @testset "Classic oracle" begin
+                oracle = ClassicalOracle(data; solver_param = typical_oracle_solver_param)
+                update_model!(oracle, data)
+                run_oracle_tests(data, oracle, test_info, [:none, :seq, :seqinout], "classical oracle")
+            end
+            
+            # Test CFLKnapsack oracle
+            @testset "CFLKnapsack oracle" begin
+                oracle = CFLKnapsackOracle(data; solver_param = typical_oracle_solver_param)
+                update_model!(oracle, data)
+                run_oracle_tests(data, oracle, test_info, [:none, :seq, :seqinout], "CFLKnapsack oracle")
+            end
+            
+            # Disjunctive oracle tests - commented out for brevity
+            # @testset "CFLKnapsack disjunctive oracle" begin
+            #     @info "solving p$i - CFLKnapsack disjunctive oracle..."
+            #     disjunctive_test_info = (mip_opt_val, master_solver_param, typical_oracle_solver_param, dcglp_solver_param, dcglp_param)
+                
+            #     # Test various parameter combinations
+            #     for strengthened in [true, false], 
+            #         add_benders_cuts_to_master in [true, false], 
+            #         reuse_dcglp in [true, false], 
+            #         p in [1.0, Inf], 
+            #         disjunctive_cut_append_rule in [NoDisjunctiveCuts(), AllDisjunctiveCuts(), DisjunctiveCutsSmallerIndices()]
+                    
+            #         @testset "strgthnd $strengthened; benders2master $add_benders_cuts_to_master; reuse $reuse_dcglp; p $p; dcut_append $disjunctive_cut_append_rule" begin
+            #             run_disjunctive_test(data, disjunctive_test_info, strengthened, add_benders_cuts_to_master, reuse_dcglp, p, disjunctive_cut_append_rule)
+            #         end
+            #     end
+            # end
+        end
+    end
+
+    instances = 1:5
+    for i in instances
+        @testset "Instance: p$i" begin
+            @info "Testing CFLP hard instance $i"
             
             # Load problem data
             problem = read_GK_data("f100-c100-r5-$i")
@@ -354,6 +409,8 @@ include("$(dirname(@__DIR__))/example/cflp/model.jl")
     end
 end
 
+
+
 # -----------------------------------------------------------------------------
 # SCFLP Tests
 # -----------------------------------------------------------------------------
@@ -373,7 +430,7 @@ end
 
 @testset verbose = true "SCFLP Sequential Benders Tests" begin
     # Specify instances to test
-    instances = 1:1  # For quick testing
+    instances = 1:5  # For quick testing
     
     for i in instances
         @testset "Instance: f25-c50-s64-r10-$i" begin
