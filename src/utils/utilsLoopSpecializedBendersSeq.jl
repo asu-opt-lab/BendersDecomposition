e002f45f448dca3e9c05ef10f3c855a22cfc8d99
gbyeon <Geunyeong.Byeon@asu.edu>
2025-04-28 10:43:46 -0700

    add lp_gap_tolerance

--- src/utils/utilsLoopSpecializedBendersSeq.jl
+++ src/utils/utilsLoopSpecializedBendersSeq.jl
@@ -3,6 +3,7 @@ export SpecializedBendersSeqParam
 mutable struct SpecializedBendersSeqParam <: AbstractBendersSeqParam
 
     time_limit::Float64
+    lp_gap_tolerance::Float64
     integrality_tolerance::Float64
     halt_limit::Int
     iter_limit::Int
@@ -10,13 +11,14 @@ mutable struct SpecializedBendersSeqParam <: AbstractBendersSeqParam
 
     function SpecializedBendersSeqParam(; 
                         time_limit::Float64 = 7200.0, 
+                        lp_gap_tolerance::Float64 = 1e-9,
                         integrality_tolerance::Float64 = 1e-9,
                         halt_limit::Int = Int(1e9), 
                         iter_limit::Int = Int(1e9), 
                         verbose::Bool = true
                         ) 
         
-        new(time_limit, integrality_tolerance, halt_limit, iter_limit, verbose)
+        new(time_limit, lp_gap_tolerance, integrality_tolerance, halt_limit, iter_limit, verbose)
     end
 end
 
