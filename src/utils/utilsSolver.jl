c9cb417769fdc55cbae4d76725d6d262e5f9d88c
kfang <kfang11@asu.edu>
2025-06-02 22:32:25 -0700

    update the whole repo

--- src/utils/utilsSolver.jl
+++ src/utils/utilsSolver.jl
@@ -1,19 +1,16 @@
 export assign_attributes!
 
-# To-Do: activate the following only when Gurobi is used?
-# const GRB_ENV = Ref{Gurobi.Env}()
-# function __init__()
-#     GRB_ENV[] = Gurobi.Env()
-#     return
-# end
+const GRB_ENV = Ref{Gurobi.Env}()
+function __init__()
+    GRB_ENV[] = Gurobi.Env()
+    return
+end
 
 function assign_attributes!(model::Model, config::Dict{String,Any})
     # Set solver based on config
     # @info config 
     if config["solver"] == "Gurobi"
         set_optimizer(model, () -> Gurobi.Optimizer(GRB_ENV[]))
-        # set_optimizer(model, Gurobi.Optimizer)
-        # set_optimizer_attribute(model, "Method", 2)
     elseif config["solver"] == "CPLEX"
         set_optimizer(model, CPLEX.Optimizer)
     else
