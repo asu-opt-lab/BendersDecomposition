2635a61052abb0b607816ea8d2c6225289b81595
inho95 <inhosin@asu.edu>
2025-06-03 12:50:09 -0700

    SNIP

--- test/5_test_callback_disjunctive/runtest.jl
+++ test/5_test_callback_disjunctive/runtest.jl
@@ -10,9 +10,9 @@ import BendersDecomposition: generate_cuts
 
 @testset "Callback Disjunctive Tests" begin
     @info "Running Callback Disjunctive Tests"
-    # include("ufl.jl")
-    # include("cfl.jl")
-    # include("scfl.jl")
+    include("ufl.jl")
+    include("cfl.jl")
+    include("scfl.jl")
     include("snip.jl")
     @info "Callback Disjunctive Tests completed"
 end
