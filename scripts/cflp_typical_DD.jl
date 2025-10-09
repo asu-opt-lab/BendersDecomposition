3cd01bece93b94b8f3554a4ed3038c35d51ea699
inho95 <inhosin@asu.edu>
2025-10-09 15:03:38 -0700

    scripts for DD

--- scripts/cflp_typical_DD.jl
+++ scripts/cflp_typical_DD.jl
@@ -2,10 +2,13 @@ using JuMP, DataFrames, Logging, CSV
 using BendersDecomposition
 using Printf  
 using Statistics  
+using Base.Threads
 import BendersDecomposition: generate_cuts
 include("$(dirname(@__DIR__))/example/cflp/data_reader.jl")
 include("$(dirname(@__DIR__))/example/cflp/oracle.jl")
 include("$(dirname(@__DIR__))/example/cflp/model.jl")
+
+@info "# of threads: $(Threads.nthreads())"
 
 
 # load settings
@@ -38,12 +41,6 @@ master_solver_param = Dict(
     "CPX_PARAM_SCRIND" => 0
 )
 
-# Basic parameter
-# master_solver_param = Dict(
-#     "solver" => "CPLEX", 
-#     "CPX_PARAM_SCRIND" => 0
-# )
-
 typical_oracle_solver_param = Dict(
     "solver" => "CPLEX",
     "CPXPARAM_Threads" => 7,
@@ -54,6 +51,10 @@ typical_oracle_solver_param = Dict(
 )
 
 # Basic parameter
+# master_solver_param = Dict(
+#     "solver" => "CPLEX", 
+#     "CPX_PARAM_SCRIND" => 0
+# )
 # typical_oracle_solver_param = Dict(
 #     "solver" => "CPLEX",
 #     "CPXPARAM_Threads" => 7,
@@ -69,13 +70,82 @@ update_model!(master, data)
 # -----------------------------------------------------------------------------
 # typical oracles
 # -----------------------------------------------------------------------------
+# Classical
+# classical_param = ClassicalOracleParam(rtol = 1e-9, atol = 1e-9)
+# typical_oracle = ClassicalOracle(data; solver_param = typical_oracle_solver_param, oracle_param = classical_param)
+# update_model!(typical_oracle, data)
+
+# Knapsack
 typical_oracle = CFLKnapsackOracle(data; solver_param = typical_oracle_solver_param)
 update_model!(typical_oracle, data)
 
 # -----------------------------------------------------------------------------
+# DD oracles
+# -----------------------------------------------------------------------------
+DD_param = DualDecompositionParam()
+DD_log = DualDecompositionLog(data)
+DD_oracle = DualDecomposition(data; typical_oracle = typical_oracle, oracle_param = DD_param, oracle_log = DD_log)
+
+# -----------------------------------------------------------------------------
+# BendersSeqInOut
+# -----------------------------------------------------------------------------
+start_time = time()
+undo = relax_integrality(master.model)
+# BendersSeq
+root_param = BendersSeqParam(
+    time_limit = 300.0,
+    gap_tolerance = 1e-6,
+    verbose = true
+)
+env = BendersSeq(data, master, DD_oracle; param = root_param)
+solution_log = solve!(env)
+println("BendersSeq done")
+
+# BendersSeqInout (DD)
+# benders_inout_param = BendersSeqInOutParam(;
+#             time_limit = 300.0,
+#             gap_tolerance = 1e-6,
+#             verbose = true,
+#             stabilizing_x = ones(data.dim_x),
+#             α = 0.9,
+#             λ = 0.1
+#             )
+# env = BendersSeqInOut(data, master, DD_oracle; param = benders_inout_param)
+# solution_log = solve!(env)
+# println("BendersSeqInOut done")
+
+# BendersSeqInout (Knapsack)
+# benders_inout_param = BendersSeqInOutParam(;
+#             time_limit = 300.0,
+#             gap_tolerance = 1e-6,
+#             verbose = true,
+#             stabilizing_x = ones(data.dim_x),
+#             α = 0.9,
+#             λ = 0.1
+#             )
+# set_optimizer_attribute(typical_oracle.model, "CPX_PARAM_LPMETHOD", 2) 
+# env = BendersSeqInOut(data, master, typical_oracle; param = benders_inout_param)
+# solution_log = solve!(env)
+# set_optimizer_attribute(typical_oracle.model, "CPX_PARAM_LPMETHOD", 0) 
+# println("BendersSeqInOut done")
+
+undo()
+
+spend_time_prev = time() - start_time
+println("Spend time: $spend_time_prev seconds")
+
+# -----------------------------------------------------------------------------
+# DD oracles
+# -----------------------------------------------------------------------------
+# DD_param = DualDecompositionParam()
+# DD_log = DualDecompositionLog(data)
+# DD_oracle = DualDecomposition(data; typical_oracle = EmptyOracle(), oracle_param = DD_param, oracle_log = DD_log)
+DD_oracle.flag_bnb = true
+
+# -----------------------------------------------------------------------------
 # lazy callback
 # -----------------------------------------------------------------------------
-lazy_callback = LazyCallback(typical_oracle)
+lazy_callback = LazyCallback(DD_oracle)
 
 # -----------------------------------------------------------------------------
 # user callback
@@ -83,34 +153,14 @@ lazy_callback = LazyCallback(typical_oracle)
 user_callback = NoUserCallback()
 
 # -----------------------------------------------------------------------------
-# BendersSeqInOut
-# -----------------------------------------------------------------------------
-start_time = time()
-undo = relax_integrality(master.model)
-benders_inout_param = BendersSeqInOutParam(;
-            time_limit = 300.0,
-            gap_tolerance = 1e-6,
-            verbose = true,
-            stabilizing_x = ones(data.dim_x),
-            α = 0.9,
-            λ = 0.1
-            )
-set_optimizer_attribute(typical_oracle.model, "CPX_PARAM_LPMETHOD", 2) 
-env = BendersSeqInOut(data, master, typical_oracle; param = benders_inout_param)
-solution_log = solve!(env)
-set_optimizer_attribute(typical_oracle.model, "CPX_PARAM_LPMETHOD", 0) 
-println("BendersSeqInOut done")
-undo()
-spend_time_prev = time() - start_time
-println("Spend time: $spend_time_prev seconds")
-# -----------------------------------------------------------------------------
 # BendersBnB
 # -----------------------------------------------------------------------------
 root_preprocessing = NoRootNodePreprocessing()
-benders_param = BendersBnBParam(
+benders_param = BendersBnBParam(;
     time_limit = 3600.0 - spend_time_prev,
     verbose = true
 )
+
 env = BendersBnB(
     data, 
     master, 
--- scripts/cflp_typical_DD.sh
+++ scripts/cflp_typical_DD.sh
@@ -2,9 +2,9 @@
 #SBATCH -t 0-01:00:00
 
 ROUND_VERSION="Dual_Decomposition"
-ROUND_DESCRIPTION="knapsack_w_strict_params"
-EXPERIMENT_VERSION="1005_knapsack_strict_lazytime"
-EXPERIMENT_DESCRIPTION="strict parameters are applied, lazy_cut_generation time printed"
+ROUND_DESCRIPTION="DD_w_strict_params"
+EXPERIMENT_VERSION="1006_DD_strict_typicalKnapsack_isapprox_50threads_T1000100051_T1500300102"
+EXPERIMENT_DESCRIPTION=" strict params are applied, noViolation(DD) runs knapsack, check isapprox between dual value and t value, 50 threads"
 
 # Define variables to make the script more readable and maintainable
 OUTPUT_DIR="experiments/${ROUND_VERSION}/${EXPERIMENT_VERSION}"
@@ -25,7 +25,7 @@ JOBSCRIPT_DIR="scripts"
 # mkdir -p "${JOBSCRIPT_DIR}"
 
 # Copy src directory to output directory
-cp -r scripts/cflp_typical_knapsack.jl "${OUTPUT_DIR}/cflp_typical_knapsack.jl"
+cp -r scripts/cflp_typical_DD.jl "${OUTPUT_DIR}/cflp_typical_DD.jl"
 
 # Create experiment metadata markdown file
 cat > "${OUTPUT_DIR}/experiment_metadata.md" << EOF
@@ -40,6 +40,7 @@ EOF
 
 # Define an array of instance names
 instances=(
+    "T1000x1000_5_1" "T1500x300_10_2"
     # "T100x100_3_1" "T100x100_3_2" "T100x100_3_3" "T100x100_3_4" "T100x100_3_5"
     # "T100x100_5_1" "T100x100_5_2" "T100x100_5_3" "T100x100_5_4" "T100x100_5_5"
     # "T100x100_10_1" "T100x100_10_2" "T100x100_10_3" "T100x100_10_4" "T100x100_10_5"
@@ -65,30 +66,30 @@ instances=(
     # "T300x300_15_1" "T300x300_15_2" "T300x300_15_3" "T300x300_15_4" "T300x300_15_5"
     # "T300x300_20_1" "T300x300_20_2" "T300x300_20_3" "T300x300_20_4" "T300x300_20_5"
 
-    "T1500x300_5_1" "T1500x300_5_2" "T1500x300_5_3" "T1500x300_5_4" "T1500x300_5_5"
-    "T1500x300_10_1" "T1500x300_10_2" "T1500x300_10_3" "T1500x300_10_4" "T1500x300_10_5"
-    "T1500x300_15_1" "T1500x300_15_2" "T1500x300_15_3" "T1500x300_15_4" "T1500x300_15_5"
-    "T1500x300_20_1" "T1500x300_20_2" "T1500x300_20_3" "T1500x300_20_4" "T1500x300_20_5"
+    # "T1500x300_5_1" "T1500x300_5_2" "T1500x300_5_3" "T1500x300_5_4" "T1500x300_5_5"
+    # "T1500x300_10_1" "T1500x300_10_2" "T1500x300_10_3" "T1500x300_10_4" "T1500x300_10_5"
+    # "T1500x300_15_1" "T1500x300_15_2" "T1500x300_15_3" "T1500x300_15_4" "T1500x300_15_5"
+    # "T1500x300_20_1" "T1500x300_20_2" "T1500x300_20_3" "T1500x300_20_4" "T1500x300_20_5"
 
     # "T500x500_5_1" "T500x500_5_2" "T500x500_5_3" "T500x500_5_4" "T500x500_5_5"
     # "T500x500_10_1" "T500x500_10_2" "T500x500_10_3" "T500x500_10_4" "T500x500_10_5"
     # "T500x500_15_1" "T500x500_15_2" "T500x500_15_3" "T500x500_15_4" "T500x500_15_5"
     # "T500x500_20_1" "T500x500_20_2" "T500x500_20_3" "T500x500_20_4" "T500x500_20_5"
 
-    "T1500x600_5_1" "T1500x600_5_2" "T1500x600_5_3" "T1500x600_5_4" "T1500x600_5_5"
-    "T1500x600_10_1" "T1500x600_10_2" "T1500x600_10_3" "T1500x600_10_4" "T1500x600_10_5"
-    "T1500x600_15_1" "T1500x600_15_2" "T1500x600_15_3" "T1500x600_15_4" "T1500x600_15_5"
-    "T1500x600_20_1" "T1500x600_20_2" "T1500x600_20_3" "T1500x600_20_4" "T1500x600_20_5"
+    # "T1500x600_5_1" "T1500x600_5_2" "T1500x600_5_3" "T1500x600_5_4" "T1500x600_5_5"
+    # "T1500x600_10_1" "T1500x600_10_2" "T1500x600_10_3" "T1500x600_10_4" "T1500x600_10_5"
+    # "T1500x600_15_1" "T1500x600_15_2" "T1500x600_15_3" "T1500x600_15_4" "T1500x600_15_5"
+    # "T1500x600_20_1" "T1500x600_20_2" "T1500x600_20_3" "T1500x600_20_4" "T1500x600_20_5"
 
     # "T700x700_5_1" "T700x700_5_2" "T700x700_5_3" "T700x700_5_4" "T700x700_5_5"
     # "T700x700_10_1" "T700x700_10_2" "T700x700_10_3" "T700x700_10_4" "T700x700_10_5"
     # "T700x700_15_1" "T700x700_15_2" "T700x700_15_3" "T700x700_15_4" "T700x700_15_5"
     # "T700x700_20_1" "T700x700_20_2" "T700x700_20_3" "T700x700_20_4" "T700x700_20_5"
 
-    "T1000x1000_5_1" "T1000x1000_5_2" "T1000x1000_5_3" "T1000x1000_5_4" "T1000x1000_5_5"
-    "T1000x1000_10_1" "T1000x1000_10_2" "T1000x1000_10_3" "T1000x1000_10_4" "T1000x1000_10_5"
-    "T1000x1000_15_1" "T1000x1000_15_2" "T1000x1000_15_3" "T1000x1000_15_4" "T1000x1000_15_5"
-    "T1000x1000_20_1" "T1000x1000_20_2" "T1000x1000_20_3" "T1000x1000_20_4" "T1000x1000_20_5"
+    # "T1000x1000_5_1" "T1000x1000_5_2" "T1000x1000_5_3" "T1000x1000_5_4" "T1000x1000_5_5"
+    # "T1000x1000_10_1" "T1000x1000_10_2" "T1000x1000_10_3" "T1000x1000_10_4" "T1000x1000_10_5"
+    # "T1000x1000_15_1" "T1000x1000_15_2" "T1000x1000_15_3" "T1000x1000_15_4" "T1000x1000_15_5"
+    # "T1000x1000_20_1" "T1000x1000_20_2" "T1000x1000_20_3" "T1000x1000_20_4" "T1000x1000_20_5"
 )
 
 # Loop through the instances and create a job script for each
@@ -102,7 +103,7 @@ for instance in "${instances[@]}"; do
     echo "#SBATCH -q grp_gbyeon" >> "${JOBSCRIPT_FILE}"
     echo "#SBATCH -N 1" >> "${JOBSCRIPT_FILE}"
     echo "#SBATCH -n 1" >> "${JOBSCRIPT_FILE}"
-    echo "#SBATCH -c 7" >> "${JOBSCRIPT_FILE}"
+    echo "#SBATCH -c 50" >> "${JOBSCRIPT_FILE}"
     # echo "#SBATCH --nodelist=pcc036" >> "${JOBSCRIPT_FILE}"
     echo "#SBATCH --mem=60G" >> "${JOBSCRIPT_FILE}"
 
@@ -117,7 +118,7 @@ for instance in "${instances[@]}"; do
     echo "module load gurobi" >> "${JOBSCRIPT_FILE}"
 
     # Run Julia script with algorithm parameters
-    echo "julia --project=. scripts/cflp_typical_knapsack.jl --instance ${instance} --output_dir ${OUTPUT_DIR}" >> "${JOBSCRIPT_FILE}"
+    echo "JULIA_NUM_THREADS=50 julia --project=. scripts/cflp_typical_DD.jl --instance ${instance} --output_dir ${OUTPUT_DIR}" >> "${JOBSCRIPT_FILE}"
 
     # Submit job
     sbatch "${JOBSCRIPT_FILE}"
