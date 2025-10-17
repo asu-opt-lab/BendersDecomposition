4790bda87256f907ac26bdf289df3c43217dc3c2
inho95 <inhosin@asu.edu>
2025-10-16 19:23:47 -0700

    SeqInOut

--- test/5_test_callback_disjunctive/cfl.jl
+++ test/5_test_callback_disjunctive/cfl.jl
@@ -105,6 +105,18 @@ include("$(dirname(dirname(@__DIR__)))/example/cflp/model.jl")
                             @test env.termination_status == Optimal()
                             @test isapprox(mip_opt_val, env.obj_value, atol=1e-5)
                         end
+                        @testset "SeqInOut" begin
+                            @info "solving CFLP p$i - disjunctive oracle/classical/seqinout - strgthnd $strengthened; benders2master $add_benders_cuts_to_master reuse $reuse_dcglp lift $lift p $p dcut_append $disjunctive_cut_append_rule adjust_t_to_fx $adjust_t_to_fx"
+                            master = Master(data; solver_param = master_solver_param)
+                            update_model!(master, data)
+                            root_preprocessing = RootNodePreprocessing(DD_oracle, BendersSeqInOut, BendersSeqInOutParam(time_limit = 300.0, gap_tolerance = 1e-6, stabilizing_x = ones(data.dim_x), α = 0.9, λ = 0.1, verbose = false))
+                            lazy_callback = LazyCallback(DD_oracle)
+                            user_callback = UserCallback(disjunctive_oracle; params=user_cb_param)
+                            env = BendersBnB(data, master, root_preprocessing, lazy_callback, user_callback; param = benders_param)
+                            log = solve!(env)
+                            @test env.termination_status == Optimal()
+                            @test isapprox(mip_opt_val, env.obj_value, atol=1e-5)
+                        end
                     end
                 end
             end
