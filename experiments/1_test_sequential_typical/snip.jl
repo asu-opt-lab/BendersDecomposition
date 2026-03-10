0d51bf86cc591265d66ca929e5a0db132305691d
inhosin <inhosin@asu.edu>
2026-03-10 09:44:16 -0700

    fix: remove @info mip obj

--- experiments/1_test_sequential_typical/snip.jl
+++ experiments/1_test_sequential_typical/snip.jl
@@ -21,7 +21,7 @@ using JuMP
             optimize!(mip_model)
             @assert termination_status(mip_model) == OPTIMAL
             mip_opt_val = objective_value(mip_model)
-            @info mip_opt_val
+
             @testset "Classic oracle" begin     
                 @info "solving SNIP instance-$instance snipno-$snipno budget-$budget - classical oracle - seq..."
                 master = Master(data; customize = customize_master_model!)
