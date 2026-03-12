f380f07aede82aeca874a9e410b68e5c6cb699f1
ASUKaiwenFang <kfang11@asu.edu>
2026-03-12 11:10:36 -0700

    fix: pareto oracle

--- src/BendersBase/src/modules/oracles/oracleTypicalPareto.jl
+++ src/BendersBase/src/modules/oracles/oracleTypicalPareto.jl
@@ -227,9 +227,9 @@ function generate_cuts(oracle::ParetoOracle, x_value::Vector{Float64}, t_value::
 
         sub_obj_val = objective_value(oracle.model)
 
-        if sub_obj_val < t_value[1] * (1 + oracle.param.rtol) + oracle.param.atol / tol_normalize
-            return true, [Hyperplane(length(x_value), length(t_value))], [sub_obj_val]
-        end
+        # if sub_obj_val < t_value[1] * (1 + oracle.param.rtol) + oracle.param.atol / tol_normalize
+        #     return true, [Hyperplane(length(x_value), length(t_value))], [sub_obj_val]
+        # end
 
         set_objective_coefficient(oracle.pareto_model, oracle.pareto_model[:σ], sub_obj_val - oracle.param.pareto_tol)
 
@@ -256,7 +256,7 @@ function generate_cuts(oracle::ParetoOracle, x_value::Vector{Float64}, t_value::
             if sub_obj_val >= t_value[1] * (1 + oracle.param.rtol) + oracle.param.atol / tol_normalize
                 return false, [Hyperplane(a_x, a_t, a_0)], [sub_obj_val]
             else
-                return true, [Hyperplane(length(x_value), length(t_value))], [sub_obj_val]
+                return true, [Hyperplane(a_x, a_t, a_0)], [sub_obj_val]
             end
         else
             throw(UnexpectedModelStatusException("ParetoOracle: Unexpected dual status $(pareto_status) for pareto_model. This is likely a numerical issue."))
