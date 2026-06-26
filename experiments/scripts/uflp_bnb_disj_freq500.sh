a99ea7e22e2cbc4faca47b65a7d637b684cea162
ASUKaiwenFang <kfang11@asu.edu>
2026-06-26 13:56:19 -0700

    refactor scripts

--- experiments/scripts/uflp_bnb_disj_freq500.sh
+++ experiments/scripts/uflp_bnb_disj_freq500.sh
@@ -7,21 +7,25 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
 cd "${REPO_ROOT}"
 
-# Define variables to make the script more readable and maintainable
-
-ROUND_VERSION="uflp_bnb_disj_freq500"
-ROUND_DESCRIPTION="freq500, vertical reverse polar norm, knapsack, 7 private cores"
-EXPERIMENT_VERSION="19"
+ROUND_VERSION="vertical_reverse_polar/uflp_knapsack"
+ROUND_DESCRIPTION="vertical_reverse_polar hard UFL, freq500, knapsack, 7 private cores, CPLEX"
+EXPERIMENT_VERSION="750_supplemental"
 SEED="1"
 HOUR="04"
-TIME_LIMIT=$((10#${HOUR} * 3600))
-FREQUENCY=500
-EXPERIMENT_DESCRIPTION="unsolved 500c instances, ${HOUR} hr, vertical reverse polar norm, LargestFractional, no lift, no reuse dcglp, branch_dir 1, 5% vbcuts to master, seed = ${SEED}"
+TIME_LIMIT="14400"
+DCGLP_TIME_LIMIT="100"
+DCGLP_ITER_LIMIT="250"
+DCGLP_HALT_LIMIT="3"
+FREQUENCY="500"
+REUSE_DCGLP="false"
+STRENGTHENED="true"
+LIFT="false"
+EXPERIMENT_DESCRIPTION="${HOUR} hr, seed = ${SEED}, vertical_reverse_polar"
 
 FILE_NAME="uflp_bnb_disj_freq500.jl"
+SHELL_FILE_NAME="uflp_bnb_disj_freq500.sh"
 THREADS=7
 
-# Define variables to make the script more readable and maintainable
 OUTPUT_DIR="experiments/${ROUND_VERSION}/${EXPERIMENT_VERSION}"
 ERR_OUT_DIR="${OUTPUT_DIR}/results"
 
@@ -30,18 +34,12 @@ if [ -d "${OUTPUT_DIR}" ]; then
     exit 1
 fi
 
-# Create necessary directories
 mkdir -p "${OUTPUT_DIR}"
 mkdir -p "${ERR_OUT_DIR}"
 
-# Define job script directory
-# JOBSCRIPT_DIR="./job_scripts"
-# mkdir -p "${JOBSCRIPT_DIR}"
-
-# Copy src directory to output directory
 cp "experiments/scripts/${FILE_NAME}" "${OUTPUT_DIR}/${FILE_NAME}"
+cp "experiments/scripts/${SHELL_FILE_NAME}" "${OUTPUT_DIR}/${SHELL_FILE_NAME}"
 
-# Create experiment metadata markdown file
 cat > "${OUTPUT_DIR}/experiment_metadata.md" << EOF
 # Experiment Metadata
 
@@ -52,64 +50,58 @@ cat > "${OUTPUT_DIR}/experiment_metadata.md" << EOF
 - **Date**: $(date "+%Y-%m-%d %H:%M:%S")
 EOF
 
-# Define an array of instance names
 instances=(
 
-    "ga250a-1" "ga250a-2" "ga250a-3" "ga250a-4" "ga250a-5"
-    "ga250b-1" "ga250b-2" "ga250b-3" "ga250b-4" "ga250b-5"
-    "ga250c-1" "ga250c-2" "ga250c-3" "ga250c-4" "ga250c-5"
+    # "ga250a-1" "ga250a-2" "ga250a-3" "ga250a-4" "ga250a-5"
+    # "ga250b-1" "ga250b-2" "ga250b-3" "ga250b-4" "ga250b-5"
+    # "ga250c-1" "ga250c-2" "ga250c-3" "ga250c-4" "ga250c-5"
 
-    "gs250a-1" "gs250a-2" "gs250a-3" "gs250a-4" "gs250a-5"
-    "gs250b-1" "gs250b-2" "gs250b-3" "gs250b-4" "gs250b-5"
-    "gs250c-1" "gs250c-2" "gs250c-3" "gs250c-4" "gs250c-5"
+    # "gs250a-1" "gs250a-2" "gs250a-3" "gs250a-4" "gs250a-5"
+    # "gs250b-1" "gs250b-2" "gs250b-3" "gs250b-4" "gs250b-5"
+    # "gs250c-1" "gs250c-2" "gs250c-3" "gs250c-4" "gs250c-5"
 
-    "ga500a-1" "ga500a-2" "ga500a-3" "ga500a-4" "ga500a-5"
-    "ga500b-1" "ga500b-2" "ga500b-3" "ga500b-4" "ga500b-5"
-    "ga500c-1" 
-    "ga500c-2" "ga500c-3" "ga500c-4" "ga500c-5"
+    # "ga500a-1" "ga500a-2" "ga500a-3" "ga500a-4" "ga500a-5"
+    # "ga500b-1" "ga500b-2" "ga500b-3" "ga500b-4" "ga500b-5"
+    # "ga500c-1" "ga500c-2" "ga500c-3" "ga500c-4" "ga500c-5"
 
-    "gs500a-1" "gs500a-2" "gs500a-3" "gs500a-4" "gs500a-5"
-    "gs500b-1" "gs500b-2" "gs500b-3" "gs500b-4" "gs500b-5"
-    "gs500c-1" "gs500c-2" 
-    "gs500c-3" "gs500c-4" "gs500c-5"
+    # "gs500a-1" "gs500a-2" "gs500a-3" "gs500a-4" "gs500a-5"
+    # "gs500b-1" "gs500b-2" "gs500b-3" "gs500b-4" "gs500b-5"
+    # "gs500c-1" "gs500c-2" "gs500c-3" "gs500c-4" "gs500c-5"
 
-    "ga750a-1" "ga750a-2" "ga750a-3" "ga750a-4" "ga750a-5"
-    "ga750b-1" "ga750b-2" "ga750b-3" "ga750b-4" "ga750b-5"
-    "ga750c-1" "ga750c-2" "ga750c-3" "ga750c-4" "ga750c-5"
+    # "ga750a-1" "ga750a-2" "ga750a-3" "ga750a-4" "ga750a-5"
+    # "ga750b-1" "ga750b-2" "ga750b-3" "ga750b-4" "ga750b-5"
+    # "ga750c-1" "ga750c-2" "ga750c-3" "ga750c-4" "ga750c-5"
 
-    "gs750a-1" "gs750a-2" "gs750a-3" "gs750a-4" "gs750a-5"
-    "gs750b-1" "gs750b-2" "gs750b-3" "gs750b-4" "gs750b-5"
-    "gs750c-1" "gs750c-2" "gs750c-3" "gs750c-4" "gs750c-5"
+    # "gs750a-1" "gs750a-2" "gs750a-3" "gs750a-4" "gs750a-5"
+    # "gs750b-1" "gs750b-2" "gs750b-3" "gs750b-4" "gs750b-5"
+    # "gs750c-1" "gs750c-2" "gs750c-3" "gs750c-4" "gs750c-5"
+
+    "ga750a-1" "gs750a-1" "gs750a-3" "gs750a-4"
 )
 
-# Loop through the instances and create a job script for each
 for instance in "${instances[@]}"; do
     JOBSCRIPT_FILE="${OUTPUT_DIR}/${instance}.sh"
-    
-    # Create job script file
+
     echo "#!/bin/bash" > "${JOBSCRIPT_FILE}"
 
+    echo "#SBATCH -p general" >> "${JOBSCRIPT_FILE}"
     echo "#SBATCH -q grp_gbyeon" >> "${JOBSCRIPT_FILE}"
     echo "#SBATCH -N 1" >> "${JOBSCRIPT_FILE}"
     echo "#SBATCH -n 1" >> "${JOBSCRIPT_FILE}"
     echo "#SBATCH -c ${THREADS}" >> "${JOBSCRIPT_FILE}"
-    echo "#SBATCH --nodelist=pcc037" >> "${JOBSCRIPT_FILE}"
+    echo "#SBATCH --nodelist=pcc036,pcc037" >> "${JOBSCRIPT_FILE}"
     echo "#SBATCH --mem=60G" >> "${JOBSCRIPT_FILE}"
 
     echo "#SBATCH -t 0-${HOUR}:30:00" >> "${JOBSCRIPT_FILE}"
     echo "#SBATCH -o ${ERR_OUT_DIR}/${instance}.out%j" >> "${JOBSCRIPT_FILE}"
     echo "#SBATCH -e ${ERR_OUT_DIR}/${instance}.err%j" >> "${JOBSCRIPT_FILE}"
 
-    # Load necessary modules
     echo "module purge" >> "${JOBSCRIPT_FILE}"
     echo "module load julia" >> "${JOBSCRIPT_FILE}"
     echo "module load cplex" >> "${JOBSCRIPT_FILE}"
-    echo "module load gurobi" >> "${JOBSCRIPT_FILE}"
 
-    # Run Julia script with algorithm parameters
-    echo "julia --project=experiments/scripts experiments/scripts/${FILE_NAME} --instance=${instance} --output_dir=${OUTPUT_DIR} --seed=${SEED} --time_limit=${TIME_LIMIT} --threads=${THREADS} --frequency=${FREQUENCY}" >> "${JOBSCRIPT_FILE}"
+    echo "julia --project=experiments/scripts experiments/scripts/${FILE_NAME} --instance=${instance} --seed=${SEED} --time_limit=${TIME_LIMIT} --dcglp_time_limit=${DCGLP_TIME_LIMIT} --dcglp_iter_limit=${DCGLP_ITER_LIMIT} --dcglp_halt_limit=${DCGLP_HALT_LIMIT} --frequency=${FREQUENCY} --threads=${THREADS} --reuse_dcglp=${REUSE_DCGLP} --strengthened=${STRENGTHENED} --lift=${LIFT}" >> "${JOBSCRIPT_FILE}"
 
-    # Submit job
-    sbatch "${JOBSCRIPT_FILE}"
+    sbatch --partition=general "${JOBSCRIPT_FILE}"
     rm "${JOBSCRIPT_FILE}"
 done
